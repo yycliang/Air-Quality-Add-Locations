1b13ab27e0d3b87f8496d5d652e8328d87d39ae8
Yong Yan (Crystal) Liang <yycliang@gmail.com>
2022-11-09 11:03:53 -0500

    final updates

--- map.js
+++ map.js
@@ -71,8 +71,146 @@ JF.getFormSubmissions("223123808962053", function (response) {
                 parameters: {
                 depthTest: false,
                 },
+                onClick: (info) => {
+                    //ADD NEW INPUT TO GETIMAGE GALLERY: 
+                    getImageGallery(info.object.fileUpload, info.object.describeWhat);
+                    flyToClick(info.object["Location Coordinates"]);
+                },
             }),
             firstLabelLayerId
         );
+        function getImagesGallery(images, text) {
+            const imageGallery = document.createElement("div");
+            imageGallery.id = "image-gallery";
+       
+            for (var i = 0; i < images.length; i++) {
+              const image = document.createElement("img");
+              image.src = images[i];
+       
+              imageGallery.appendChild(image);
+            }
+       
+            //   add exit button to image gallery
+            const exitButton = document.createElement("button");
+            exitButton.id = "exit-button";
+            exitButton.innerHTML = "X";
+            exitButton.addEventListener("click", () => {
+              document.getElementById("image-gallery").remove();
+            });
+       
+            //   stylize the exit button to look good: this can also be a css class
+            exitButton.style.position = "fixed";
+            exitButton.style.top = "0";
+            exitButton.style.right = "0";
+            exitButton.style.borderRadius = "0";
+            exitButton.style.padding = "1rem";
+            exitButton.style.fontSize = "2rem";
+            exitButton.style.fontWeight = "bold";
+            exitButton.style.backgroundColor = "white";
+            exitButton.style.border = "none";
+            exitButton.style.cursor = "pointer";
+              exitButton.style.zIndex = "1";
+       
+       
+            imageGallery.appendChild(exitButton);
+       
+            // add text to image gallery
+            const textDiv = document.createElement("div");
+            textDiv.id = "image-gallery-text";
+            textDiv.innerHTML = text;
+        
+            // add fixed styling if in modal view
+            textDiv.style.position = "fixed";
+            textDiv.style.top = "0";
+            textDiv.style.left = "0";
+            textDiv.style.right = "0";
+            textDiv.style.borderRadius = "0";
+            textDiv.style.padding = "2rem";
+            textDiv.style.fontSize = "2rem";
+        
+            imageGallery.appendChild(textDiv);
+        
+            // append the image gallery to the body
+            document.body.appendChild(imageGallery);
+            }
+        function flyToClick(coords) {
+            map.flyTo({
+                center: [coords[0], coords[1]],
+                zoom: 17,
+                essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+            });
+        }      
+        // create “current location” function, which doesn’t trigger until called upon.
+    function addUserLocation(latitude, longitude) {
+        return map.addLayer(
+          new MapboxLayer({
+            id: "user-location",
+            type: ScatterplotLayer,
+            data: [{ longitude, latitude }],
+            getPosition: (d) => [d.longitude, d.latitude],
+            getSourceColor: [0, 255, 0],
+            sizeScale: 15,
+            getSize: 10,
+            radiusUnits: "pixels",
+            getRadius: 5,
+            opacity: 0.7,
+            stroked: false,
+            filled: true,
+            radiusScale: 3,
+            getFillColor: [3, 202, 252],
+            parameters: {
+              depthTest: false,
+            },
+          })
+        );
+      }
+   
+      // get current location
+      const successCallback = (position) => {
+        // add new point layer of current location to deck gl
+        const { latitude, longitude } = position.coords;
+        addUserLocation(latitude, longitude);
+      };
+   
+      const errorCallback = (error) => {
+        console.log(error);
+      };
+   
+  // create async function to await for current location and then return the promise as lat long coordinates then resolve the promise
+      function getCurrentLocation() {
+        const currentLocation = navigator.geolocation.getCurrentPosition(
+          successCallback,
+          errorCallback
+        );
+        return currentLocation;
+      }
+      if (navigator.geolocation) {
+        getCurrentLocation();
+      }
+      const locationButton = document.createElement("div");
+      // create a button that will request the users location
+      locationButton.textContent = "Where am I?";
+      locationButton.id = "location-button";
+      locationButton.addEventListener("click", () => {
+        // when clicked, get the users location
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+   
+            locationButton.textContent =
+              "Where am I? " +
+              position.coords.latitude.toFixed(3) +
+              ", " +
+              position.coords.longitude.toFixed(3);
+   
+            addUserLocation(latitude, longitude);
+            flyToClick([longitude, latitude]);
+          });
+        }
+      });
+   
+      // append the button
+      document.body.appendChild(locationButton);
+  
     }); 
 });
